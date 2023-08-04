bfc04e80ec0a9a576a667cc0e7b5d9eeab7bf640
unknown <sherpatpemba11@gmail.com>
2023-08-04 15:07:27 +0545

    modified2

--- filesystem4.py
+++ filesystem4.py
@@ -132,8 +132,12 @@
 #     main()
 
 import os
+import zipfile
+import shutil
+from cryptography.fernet import Fernet
 
-import os
+filename = None
+directory_name=None
 
 def create_file(filename, content):
     try:
@@ -186,6 +190,86 @@ def delete_directory(directory_name):
         return True, "Directory deleted successfully!"
     except Exception as e:
         return False, str(e)
+   
+
+def compress_file(filename):
+    try:
+        with zipfile.ZipFile(f"{filename}.zip", 'w') as zip_file:
+            zip_file.write(filename, os.path.basename(filename))
+        return True, "File compressed successfully!"
+    except Exception as e:
+        return False, str(e)
+
+def decompress_file(filename):
+    try:
+        with zipfile.ZipFile(filename, 'r') as zip_file:
+            zip_file.extractall(os.path.dirname(filename))
+        return True, "File decompressed successfully!"
+    except Exception as e:
+        return False, str(e)
+    
+
+def compress_folder(foldername):
+    try:
+        shutil.make_archive(foldername, 'zip', foldername)
+        return True, "Folder compressed successfully!"
+    except Exception as e:
+        return False, str(e)
+    
+def decompress_folder(zip_filename):
+    try:
+        folder_path = os.path.splitext(zip_filename)[0]
+        with zipfile.ZipFile(zip_filename, 'r') as zip_file:
+            zip_file.extractall(folder_path)
+        return True, "Folder decompressed successfully!"
+    except Exception as e:
+        return False, str(e)
+
+def rename_file(old_name, new_name):
+    try:
+        os.rename(old_name, new_name)
+        return True, "File renamed successfully!"
+    except Exception as e:
+        return False, str(e)
+
+def rename_directory(old_name, new_name):
+    try:
+        os.rename(old_name, new_name)
+        return True, "Directory renamed successfully!"
+    except Exception as e:
+        return False, str(e)
+
+
+def encrypt_file(filename, key):
+    try:
+        with open(filename, 'rb') as file:
+            data = file.read()
+
+        fernet = Fernet(key)
+        encrypted_data = fernet.encrypt(data)
+
+        with open(filename, 'wb') as file:
+            file.write(encrypted_data)
+
+        return True, "File encrypted successfully!"
+    except Exception as e:
+        return False, str(e)
+
+def decrypt_file(filename, key):
+    try:
+        with open(filename, 'rb') as file:
+            encrypted_data = file.read()
+
+        fernet = Fernet(key)
+        decrypted_data = fernet.decrypt(encrypted_data)
+
+        with open(filename, 'wb') as file:
+            file.write(decrypted_data)
+
+        return True, "File decrypted successfully!"
+    except Exception as e:
+        return False, str(e)
+    
 
 
 def main_menu():
@@ -195,15 +279,19 @@ def main_menu():
     print("3. Exit")
     return input("Enter the option number: ")
 
-def file_options():
+def file_options(filename):
     print("\nFile Options:")
     print("1. Create a file")
     print("2. Read a file")
     print("3. Update a file")
     print("4. Delete a file")
-    print("5. Back to Main Menu")
+    print("5. Compress a file")
+    print("6. Decompress a file")
+    print("7. Rename a file")
+    print("8. Encrypt file ")
+    print("9. Decrypt file")
+    print("10. Back to Main Menu")
     choice = input("Enter the file operation number: ")
-
     if choice == '1':
         filename = input("Enter the filename: ")
         content = input("Enter the content to write to the file: ")
@@ -221,17 +309,43 @@ def file_options():
         filename = input("Enter the filename: ")
         success, message = delete_file(filename)
     elif choice == '5':
-        return
+        filename = input("Enter the filename to compress: ")
+        success, message = compress_file(filename)
+    elif choice == '6':
+        filename = input("Enter the filename to decompress: ")
+        success, message = decompress_file(filename)
+    elif choice == '7':
+        # Rename a file
+        # Ask for old and new directory names
+        old_name = input("Enter the old file name: ")
+        new_name = input("Enter the new file name: ")
+        success, message = rename_file(old_name, new_name)
+        print(message)
+        # Call file_options() recursively with file_name
+        file_options(filename)
+    elif choice == '8':
+        # Encrypt a file
+        key = Fernet.generate_key()
+        success, message = encrypt_file(filename, key)
+    elif choice == '9':
+    # Decrypt a file
+      key = input("Enter the encryption key: ")
+      success, message = decrypt_file(filename, key)
+
 
     print(message)
     file_options()
 
-def directory_options():
+def directory_options(directory_name=None):
     print("\nDirectory Options:")
     print("1. Create a directory")
     print("2. List items in a directory")
     print("3. Delete a directory")
-    print("4. Back to Main Menu")
+    print("4. Compress a directory")
+    print("5. Decompress a directory")
+    print("6. Rename a directory")
+    
+    print("7. Back to Main Menu")
     choice = input("Enter the directory operation number: ")
 
     if choice == '1':
@@ -248,7 +362,23 @@ def directory_options():
         directory_name = input("Enter the directory name: ")
         success, message = delete_directory(directory_name)
     elif choice == '4':
-        return
+        # Compress a folder
+        foldername = input("Enter the folder name to compress: ")
+        success, message = compress_folder(foldername)
+    elif choice == '5':
+        # Decompress a folder
+        zip_filename = input("Enter the zip filename to decompress: ")
+        success, message = decompress_folder(zip_filename)#attention required
+    elif choice == '6':
+        # Rename a directory
+        # Ask for old and new directory names
+        old_name = input("Enter the old directory name: ")
+        new_name = input("Enter the new directory name: ")
+        success, message = rename_directory(old_name, new_name)
+        print(message)
+        # Call directory_options() recursively with directory_name
+        directory_options(directory_name)
+      
 
     print(message)
     directory_options()
@@ -260,7 +390,7 @@ def main():
         choice = main_menu()
 
         if choice == '1':
-            file_options()
+            file_options(  )
         elif choice == '2':
             directory_options()
         elif choice == '3':
